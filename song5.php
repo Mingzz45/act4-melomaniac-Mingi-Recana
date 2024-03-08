92b0d4c31aea5ed6bc9c1b652d4374711bcea0cf
MingiStephannie <stepmingi@gmail.com>
2024-03-09 02:54:41 +0800

    song5 with lyrics

---
+++ song5.php
@@ -0,0 +1,188 @@
+<!DOCTYPE <html>
+<html>
+</head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>song5</title>
+    <style>
+        body {
+            background-image: url(bg2.jpg);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', sans-serif;
+            background-color: #f2f2f2;
+            color: #333;
+            line-height: 1.15;
+            background-size: cover;
+            background-attachment: fixed;
+            font-size: 45;
+            
+        }
+        .content {
+            text-align: center;
+        }
+
+        h1 {
+            color: #ffffff;
+            -webkit-text-stroke:2px #000 ;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+            text-align: center;
+        }
+
+        li {
+            margin: 5px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #f3f;
+            font-weight: bold;
+            font-size: 30;
+        }
+
+        a:hover {
+            color: #ffffff;
+        }
+        p {
+            color: #ffffff;
+            margin-top: 1;
+            margin-bottom: 1;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Paninindigan Kita</h1>
+    <?php
+echo "<p>STRUMMING<p>";
+echo "<p>There is no strumming pattern for this song yet. Create and get +5 IQ<p>";
+echo "<p>[Intro]<p>";
+echo "<p>G D G D<p>";
+echo "<br>";   
+echo "<br>";  
+echo "<p>[Verse 1]<p>";
+echo "<p>    Gmaj7                 Dmaj7<p>";
+echo "<p>Kakainis na ayaw mong maniwalang mahal kita<p>";
+echo "<p>       Gmaj7                   Dmaj7<p>";
+echo "<p>Ano pa bang kailangan kong patunayan sa'yo sinta?<p>";
+echo "<p>       Em7                     Dmaj7      Bm7<p>";
+echo "<p>Pero naiintindihan ko naman, iniiwasan mo lang masaktan<p>";
+echo "<p>    Em7                       A      D     A<p>";
+echo "<p>'Di ka madedehado, kung sasagutin mo lang ako<p>";
+echo "<br>";  
+echo "<br>";   
+echo "<p>[Chorus]<p>";
+echo "<p>         D<p>";
+echo "<p>Paninindigan kita, oo<p>";
+echo "<p>      F#m<p>";
+echo "<p>Mamahalin kitang buong-buo<p>";
+echo "<p>Em7                         D<p>";
+echo "<p>Kahit sa pagtanda, ako'y sa'yo<p>";
+echo "<p>         Gmaj7<p>";
+echo "<p>Paninindigan kita, oo<p>";
+echo "<p>          F#m7<p>";
+echo "<p>Anumang sabihin ng magulong mundo<p>";
+echo "<p>Em7                        Bm7<p>";
+echo "<p>Kahit ayaw nilang ako'y sa'yo<p>";
+echo "<p>A              Gmaj7 Dmaj7<p>";
+echo "<p>Ika'y iingatan ko<p>";
+echo "<br>";   
+echo "<br>";   
+echo "<p>[Verse 2]<p>";
+echo "<p>      Gmaj7                Dmaj7<p>";
+echo "<p>Kakakilig ka sa tuwing lumalapit ka na sinta<p>";
+echo "<p>      Gmaj7                  Dmaj7<p>";
+echo "<p>Nawawala ang angas ko, pilit man na itago pa<p>";
+echo "<p>              Em7<p>";
+echo "<p>Mm, grabe kasi kung pa'no mo 'ko itahan<p>";
+echo "<p>  D            Bm<p>";
+echo "<p>Kapag napupuno na 'ko<p>";
+echo "<p>         Em7                        A<p>";
+echo "<p>Sa ingay ng paligid, ikaw ang aking katahimikan<p>";
+echo "<br>";   
+echo "<br>";   
+echo "<p>[Chorus]<p>";
+echo "<p>         D<p>";
+echo "<p>Paninindigan kita, oo<p>";
+echo "<p>      F#m<p>";
+echo "<p>Mamahalin kitang buong-buo<p>";
+echo "<p>Em7                         D<p>";
+echo "<p>Kahit sa pagtanda, ako'y sa'yo<p>";
+echo "<p>         Gmaj7<p>";
+echo "<p>Paninindigan kita, oo<p>";
+echo "<p>          F#m7<p>";
+echo "<p>Anumang sabihin ng magulong mundo<p>";
+echo "<p>Em7                        Bm7<p>";
+echo "<p>Kahit ayaw nilang ako'y sa'yo<p>";
+echo "<p>A              Gmaj7 F#m7<p>";
+echo "<p>Ika'y iingatan ko<p>";
+echo "<p>         Em7                 Bm7 A<p>";
+echo "<p>Paninindigan kita, ah-ah, ah-ah<p>";
+echo "<br>";   
+echo "<br>";   
+echo "<p>[Bridge]<p>";
+echo "<p>    Em7                 F#m7<p>";
+echo "<p>Sasamahan ka kahit na napapalibutan ng mga problema<p>";
+echo "<p>   Em7                   A<p>";
+echo "<p>Sa hirap at ginhawa, dadamayan kita, sinta<p>";
+echo "<p>         G<p>";
+echo "<p>Paninindigan kita, oo<p>";
+echo "<p>       F#m7<p>";
+echo "<p>Kahit alam kong, tayo'y magbabago<p>";
+echo "<p>Em7                       D<p>";
+echo "<p>Magmula umpisa hanggang dulo<p>";
+echo "<p>         G<p>";
+echo "<p>Paninindigan kita<p>";
+echo "<p>         F#m7<p>";
+echo "<p>Paninindigan kita<p>";
+echo "<p>              Em7<p>";
+echo "<p>'Wag kang mag-alala<p>";
+echo "<p>              A<p>";
+echo "<p>Ako'y siguradong-sigurado<p>";
+echo "<br>";   
+echo "<br>";   
+echo "<p>[Chorus]<p>";
+echo "<p>         D<p>";
+echo "<p>Paninindigan kita, oo<p>";
+echo "<p>      F#m<p>";
+echo "<p>Mamahalin kitang buong-buo<p>";
+echo "<p>Em7                         D<p>";
+echo "<p>Kahit sa pagtanda, ako'y sa'yo<p>";
+echo "<p>         Gmaj7<p>";
+echo "<p>Paninindigan kita, oo<p>";
+echo "<p>          F#m7<p>";
+echo "<p>Anumang sabihin ng magulong mundo<p>";
+echo "<p>Em7                        Bm7<p>";
+echo "<p>Kahit ayaw nilang ako'y sa'yo<p>";
+echo "<p>A              Gmaj7<p>";
+echo "<p>Ika'y iingatan ko<p>";
+echo "<br>";   
+echo "<br>";   
+echo "<p>[Outro]<p>";
+echo "<p>         F#m7<p>";
+echo "<p>Paninindigan kita, ah-ah-ah<p>";
+echo "<p>         Em7<p>";
+echo "<p>Paninindigan kita<p>";
+echo "<p>         Bm7<p>";
+echo "<p>Paninindigan kita<p>";
+echo "<p>A              Gmaj7<p>";
+echo "<p>Ika'y iingatan ko (Paninindigan kita)<p>";
+echo "<p>            F#m7<p>";
+echo "<p>At aalagaan ko (Paninindigan kita)<p>";
+echo "<p>                     Em7<p>";
+echo "<p>Kung andito na rin tayo (Paninindigan kita)<p>";
+echo "<p>       A                D<p>";
+echo "<p>Panindigan na natin 'to<p>";
+
+?>
+<a href="index.php">Go Back</a>
+    </body>
+</html>
