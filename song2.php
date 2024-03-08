7e3c14e36ac140cfa01d9580143b4fa901af2474
MingiStephannie <stepmingi@gmail.com>
2024-03-09 02:52:38 +0800

    song2 with lyrics

---
+++ song2.php
@@ -0,0 +1,178 @@
+<!DOCTYPE <html>
+<html>
+</head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>song2</title>
+    <style>
+        body {
+            background-image: url(bg2.jpg);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', sans-serif;
+            background-color: #f3f;
+            color: #333;
+            line-height: 1.15;
+            background-size: cover;
+            background-attachment: fixed;
+            font-size: 45;
+            
+        }
+        .content {
+            text-align: center;
+        }
+
+        h1 {
+            color: #ffffff;
+            -webkit-text-stroke:2px #000 ;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+            text-align: center;
+        }
+
+        li {
+            margin: 5px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #f3f;
+            font-weight: bold;
+            font-size: 30;
+        }
+
+        a:hover {
+            color: #ffffff;
+        }
+
+        p {
+            color: #ffffff;
+            margin-top: 1;
+            margin-bottom: 1;
+        }
+    </style>
+</head>
+<body>
+    <h1>Take All The Love</h1>
+    <?php
+echo "<p>STRUMMING<p>";
+echo "<p>There is no strumming pattern for this song yet. Create and get +5 IQ<p>";
+echo "<p>Tip for strumming: You can basically make your
+own strumming.<p>";
+echo "<br>"; 
+echo "<p>[Verse 1]<p>";
+echo "<p>Dm7              Am7   Gm    F     C<p>";
+echo "<p>    You'll be safe in my romance, babe<p>";
+echo "<p>Dm7         Am7<p>";
+echo "<p>    Label's fading<p>";
+echo "<p>Gm                     F      C<p>";
+echo "<p>But I see you cravin' for more<p>";
+echo "<p>Dm7           Am7<p>";
+echo "<p>    You're denying it<p>";
+echo "<p>Gm                F            C<p>";
+echo "<p>But you ain't controlling your actions<p>";
+echo "<p>Dm7         Am7<p>";
+echo "<p>   Keep me guessing<p>";
+echo "<p>Gm                       F<p>";
+echo "<p>But don't keep me waiting for us<p>";
+echo "<br>"; 
+echo "<p>[Chorus]<p>";
+echo "<p>      Dm7       Am7<p>";
+echo "<p>Oh I live to appreciate you<p>";
+echo "<p>Gm                       F    C<p>";
+echo "<p>Take all the love that I give<p>";
+echo "<p>          Dm7          Am7<p>";
+echo "<p>All that I wish is for you to take me<p>";
+echo "<p>Gm                              F  C<p>";
+echo "<p>But I taste the lies on your lips<p>";
+echo "<p>              Dm7                 Am7<p>";
+echo "<p>I count the days when we're in love<p>";
+echo "<p>               Gm                     F  C<p>";
+echo "<p>'Cuz I only matter when he's not around<p>";
+echo "<p>          Dm7          Am7<p>";
+echo "<p>Oh but I live to appreciate you<p>";
+echo "<p>Gm                           F  C<p>";
+echo "<p>So take all the love that I give<p>";
+echo "<br>"; 
+echo "<p>       Dm7       Am7        GM       F C<p>";
+echo "<p>Ohhhhh, Ooooooooh, Ooooooooh, Ooooooohhh<p>";
+echo "<br>"; 
+echo "<p>[Verse 2]<p>";
+echo "<p>Dm7           Am7<p>";
+echo "<p>     Got my heart on my sleeve<p>";
+echo "<p>Gm                  F              C<p>";
+echo "<p>I'm beggin' the heavens to to be good to me<p>";
+echo "<p>Dm7                 Am7<p>";
+echo "<p>      Are you obsessive love?<p>";
+echo "<p> Gm          F            C         Dm7<p>";
+echo "<p>'Cuz I'm tolerating all of your desires<p>";
+echo "<p>    Am7                     Gm<p>";
+echo "<p>I'll keep you wanting me more oh<p>";
+echo "<p>            F           C<p>";
+echo "<p>Leave but I'll keep you comin'<p>";
+echo "<p>Dm7  Am7          Gm                     F   C<p>";
+echo "<p>   I'm sure it's my love that got you suffocated<p>";
+echo "<br>"; 
+echo "<p>[Chorus]<p>";
+echo "<p>      Dm7          Am7<p>";
+echo "<p>Oh I live to appreciate you<p>";
+echo "<p>Gm                         F  C<p>";
+echo "<p>Take all the love that I give<p>";
+echo "<p>            Dm7        Am7<p>";
+echo "<p>All that I wish is for you to take me<p>";
+echo "<p>Gm                            F  C<p>";
+echo "<p>But I taste the lies on your lips<p>";
+echo "<p>             Dm7                 Am7<p>";
+echo "<p>I count the days when we're in love<p>";
+echo "<p>Gm                                    F  C<p>";
+echo "<p>'Cuz I only matter when he's not around<p>";
+echo "<p>          Dm7         Am7<p>";
+echo "<p>Oh but I live to appreciate you<p>";
+echo "<p>Gm                            F   C<p>";
+echo "<p>So take all the love that I give<p>";
+echo "<br>"; 
+echo "<p>[Bridge]<p>";
+echo "<p>Dm7          Am7<p>";
+echo "<p>You come over but not too often<p>";
+echo "<p> Gm                   F             C<p>";
+echo "<p>You don't visit 'cuz you got more options<p>";
+echo "<br>"; 
+echo "<p>Dm7          Am7<p>";
+echo "<p>You come over but not too often<p>";
+echo "<p> Gm                   F             C<p>";
+echo "<p>You don't visit 'cuz you got more options<p>";
+echo "<br>"; 
+echo "<p>Dm7          Am7<p>";
+echo "<p>You come over but not too often<p>";
+echo "<p> Gm                   F             C<p>";
+echo "<p>You don't visit 'cuz you got more options<p>";
+echo "<br>";  
+echo "<p>[Chorus]<p>";
+echo "<p>      Dm7          Am7<p>";
+echo "<p>Oh I live to appreciate you<p>";
+echo "<p>Gm                         F  C<p>";
+echo "<p>Take all the love that I give<p>";
+echo "<p>            Dm7        Am7<p>";
+echo "<p>All that I wish is for you to take me<p>";
+echo "<p>Gm                            F  C<p>";
+echo "<p>But I taste the lies on your lips<p>";
+echo "<p>             Dm7                 Am7<p>";
+echo "<p>I count the days when we're in love<p>";
+echo "<p>Gm                                    F  C<p>";
+echo "<p>'Cuz I only matter when he's not around<p>";
+echo "<p>          Dm7         Am7<p>";
+echo "<p>Oh but I live to appreciate you<p>";
+echo "<p>Gm                            F   C<p>";
+echo "<p>So take all the love that I give<p>";
+
+?>
+<a href="index.php">Go Back</a>
+    </body>
+</html>
