59a287d1d0ecbae727171b9c34085c9174f1d152
MingiStephannie <stepmingi@gmail.com>
2024-03-09 02:53:14 +0800

    song3 with lyrics

---
+++ song3.php
@@ -0,0 +1,195 @@
+<!DOCTYPE <html>
+<html>
+</head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>song3</title>
+    <style>
+        body {
+            background-image: url(bg2.jpg);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', sans-serif;
+            background-color: #f3f;
+            color: #333;
+            line-height: 1.15;
+            background-size: cover;
+            background-attachment: fixed;
+            font-size: 45;
+            
+        }
+        .content {
+            text-align: center;
+        }
+
+        h1 {
+            color: #fff;
+            -webkit-text-stroke:2px #000 ;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+            text-align: center;
+        }
+
+        li {
+            margin: 5px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #f3f;
+            font-weight: bold;
+            font-size: 30;
+        }
+
+        a:hover {
+            color: #fff;
+        }
+
+        p {
+            color: #fff;
+            margin-top: 1;
+            margin-bottom: 1;
+        }
+    </style>
+</head>
+<body>
+    <h1>Talking To The Moon</h1>
+    <?php
+echo "<p>STRUMMING<p>";
+echo "<br>"; 
+echo "<p>[Intro]<p>";
+echo "<br>";  
+echo "<p>| C   | C   |<p>";
+echo "<br>";  
+echo "<br>"; 
+echo "<p>[Verse 1]<p>";
+echo "<br>";  
+echo "<p>C<p>";
+echo "<p>  I know you're somewhere out there<p>";
+echo "<p>E7<p>";
+echo "<p>Somewhere far away<p>";
+echo "<p>  Am           G<p>";
+echo "<p>I want you back<p>";
+echo "<p>  F<p>";
+echo "<p> I want you back<p>";
+echo "<br>";  
+echo "<p> C<p>";
+echo "<p>   My neighbors think I'm crazy<p>";
+echo "<p>     E7<p>";
+echo "<p> But they don't understand<p>";
+echo "<p>        Am        G<p>";
+echo "<p> You're all I had<p>";
+echo "<p>        F<p>";
+echo "<p> You're all I had<p>";
+echo "<br>";  
+echo "<br>";  
+echo "<p> [Pre-Chorus]<p>";
+echo "<br>";  
+echo "<p> Dm                      G<p>";
+echo "<p>    At night when the stars<p>";
+echo "<p>                Dm<p>";
+echo "<p> light up my room<p>";
+echo "<p>             G<p>";
+echo "<p> I sit by myself<p>";
+echo "<br>";  
+echo "<br>";  
+echo "<p> [Chorus]<p>";
+echo "<br>";  
+echo "<p>                F G Am<p>";
+echo "<p> Talking to the moon<p>";
+echo "<p>                F G Am<p>";
+echo "<p> Tryna get to you<p>";
+echo "<p>    F                    G         C       G/B   Am<p>";
+echo "<p>In hopes you're on the other side talking to me too<p>";
+echo "<p>   G      F              G<p>";
+echo "<p>Or am I a fool who sits alone<p>";
+echo "<p>                Am  G<p>";
+echo "<p>Talking to the moon<p>";
+echo "<p>     D<p>";
+echo "<p>Ohhhhhhhhhhhhhhhhhhhhhh<p>";
+echo "<br>";  
+echo "<br>";  
+echo "<p>[Verse 2]<p>";
+echo "<br>";  
+echo "<p>C<p>";
+echo "<p>  I'm feelin' like I'm famous<p>";
+echo "<p>    E7<p>";
+echo "<p>The talk of the town<p>";
+echo "<p>         Am           G<p>";
+echo "<p>They say I've gone mad<p>";
+echo "<p>      F<p>";
+echo "<p>Yeah, I've gone mad<p>";
+echo "<br>";  
+echo "<p>C<p>";
+echo "<p>  But they don't know what I know<p>";
+echo "<p>      E7<p>";
+echo "<p>Cause when the sun goes down<p>";
+echo "<p>          Am           G<p>";
+echo "<p>Someone's talkin' back<p>";
+echo "<p>              F<p>";
+echo "<p>Yeah, they're talkin' back, ohhh<p>";
+echo "<br>"; 
+echo "<br>";  
+echo "<p>[Pre-Chorus]<p>";
+echo "<br>";  
+echo "<p>Dm                      G<p>";
+echo "<p>   At night when the stars<p>";
+echo "<p>               Dm<p>";
+echo "<p>light up my room<p>";
+echo "<p>            G<p>";
+echo "<p>I sit by myself<p>";
+echo "<br>";  
+echo "<br>";  
+echo "<p>[Chorus]<p>";
+echo "<br>";  
+echo "<p>               F G Am<p>";
+echo "<p>Talking to the moon<p>";
+echo "<p>              F G Am<p>";
+echo "<p>Tryna get to you<p>";
+echo "<p>   F                    G         C       G/B   Am<p>";
+echo "<p>In hopes you're on the other side talking to me too<p>";
+echo "<p>   G      F              G<p>";
+echo "<br>";  
+echo "<p>Talking to the moon<p>";
+echo "<br>";  
+echo "<br>";  
+echo "<p>[Interlude]<p>";
+echo "<br>";  
+echo "<p>Dm      C      Dm<p>";
+echo "<p>  Ahh... ahh... ahh...<p>";
+echo "<p>G<p>";
+echo "<p>Do you ever hear me calling?<p>";
+echo "<p>Dm      C      Dm<p>";
+echo "<p>  (Ahh... ahh... ahh...)<p>";
+echo "<br>";  
+echo "<br>";  
+echo "<p>[Chorus]<p>";
+echo "<br>";  
+echo "<p>      G                              F G Am<p>";
+echo "<p>Cause every night I'm talking to the moon<p>";
+echo "<p>                       F G Am<p>";
+echo "<p>Still tryna to get to you<p>";
+echo "<p>   F                    G         C       G/B   Am<p>";
+echo "<p>In hopes you're on the other side talking to me too<p>";
+echo "<p>   G      F              G                     Am   G    D<p>";
+echo "<p>Or am I a fool who sits alone talking to the moon?     Ohoooo...<p>";
+echo "<br>";  
+echo "<br>";  
+echo "<p>[Outro]<p>";
+echo "<br>";  
+echo "<p>C<p>";
+echo "<p>  I know you're somewhere out there<p>";
+echo "<p>  E7<p>";
+echo "<p>Somewhere far away<p>";
+
+?>
+<a href="index.php">Go Back</a>
+    </body>
+</html>
