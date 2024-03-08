78d4439ea39f8ed7405aa1c884977381f9488665
MingiStephannie <stepmingi@gmail.com>
2024-03-09 02:51:56 +0800

    song1 with lyrics

---
+++ song1.php
@@ -0,0 +1,223 @@
+<!DOCTYPE <html>
+<html>
+</head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>song1</title>
+    <style>
+        body {
+            background-image: url(bg2.jpg);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', sans-serif;
+            background-color: #f3f;
+            color: #333;
+            line-height: 1.15;
+            background-size: cover;
+            background-attachment: fixed;
+            font-size: 45;
+        }
+        .content {
+            text-align: center;
+        }
+
+        h1 {
+            color: #ffffff;
+            -webkit-text-stroke:2px #000 ;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+            text-align: center;
+           
+        }
+
+        li {
+            margin: 5px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #f3f;
+            font-weight: bold;
+            font-size: 30;
+        }
+
+        a:hover {
+            color: #ffffff;
+        }
+
+        p {
+            color: #ffffff;
+            margin-top: 1;
+            margin-bottom: 1;
+        }
+    </style>
+</head>
+<body>
+    <h1>Statue</h1>
+    <?php
+echo "<p>STRUMMING<p>";
+echo "<p>There is no strumming pattern for this song yet. Create and get +5 IQ<p>";
+echo "<p>Orig. Key: C#<p>";
+echo "<br>"; 
+echo "<p>Capo 1 (Key: C)<p>";
+echo "<br>"; 
+echo "<p>[Intro]<p>";
+echo "<p>C Am F G (2x)<p>";
+echo "<br>"; 
+echo "<p>[Verse 1]<p>";
+echo"<p>       C<p>";
+echo "<p>When a day is said and done<p>";
+echo "<p>                    Am<p>";
+echo "<p>In the middle of the night and your fast asleep, my love.<p>";
+echo "<p>      F<p>";
+echo "<p>Stay awake looking at your beauty<p>";
+echo "<p>         G<p>";
+echo "<p>telling myself Im the luckiest man alive<p>";
+echo "<p>      C                                         Am<p>";
+echo "<p>Cause so many times I was certain you was gonna walk out of my life (life)<p>";
+echo "<p>F<p>";
+echo "<p>Why you take such a hold of me girl<p>";
+echo "<p>        G<p>";
+echo "<p>when Im still trying to get my act right.<p>";
+echo "<br>"; 
+echo "<p>[Pre-Chorus]<p>";
+echo "<p>F                                              G<p>";
+echo "<p>What is the reason, when you really could have any man you want<p>";
+echo "<p>Am                C         G/B<p>";
+echo "<p>I don't see, what I have to offer.<p>";
+echo "<p> F                                           G<p>";
+echo "<p>I shouldn't be in season, guess you could see I had potential.<p>";
+echo "<p>Fmaj7<p>";
+echo "<p>Do you know your my miracle?<p>";
+echo "<br>"; 
+echo "<p>[Chorus]<p>";
+echo "<p>             C                     Am<p>";
+echo "<p>’m like a statue, stuck staring right at you<p>";
+echo "<p>                    F<p>";
+echo "<p>Got me frozen in my tracks.<p>";
+echo "<p>So amazed how you take me back<p>";
+echo "<p>G<p>";
+echo "<p>Each and everytime our love collapsed<p>";
+echo "<p>C                     Am<p>";
+echo "<p>Statue, stuck staring right at you<p>";
+echo "<p>            F<p>";
+echo "<p>So when I’m lost for words<p>";
+echo "<p>                      G<p>";
+echo "<p>Everytime I disappoint you<p>";
+echo "<p>Its just cause I can’t believe<p>";
+echo "<p>            C        Am<p>";
+echo "<p>That you're so beautiful. (Stuck like a statue)<p>";
+echo "<p>      F               G<p>";
+echo "<p>Don't wanna lose you, no. (Stuck like a statue)<p>";
+echo "<br>"; 
+echo "<p>[Verse 2]<p>";
+echo "<p>          C<p>";
+echo "<p>Ask myself why are you even with me<p>";
+echo "<p>      Am<p>";
+echo "<p>After all the shit I put you through<p>";
+echo "<p>           F<p>";
+echo "<p>Why did you make your heart song with me<p>";
+echo "<p>                G<p>";
+echo "<p>It’s like you're living in an igloo<p>";
+echo "<p>   C                                     Am<p>";
+echo "<p>But baby your love is so warm it makes my shield melt down (down)<p>";
+echo "<p>    F<p>";
+echo "<p>And everytime were both at war<p>";
+echo "<p>           G<p>";
+echo "<p>You make me come around.<p>";
+echo "<p>           G<p>";
+echo "<p>when Im still trying to get my act right.<p>";
+echo "<p>[Pre-Chorus]<p>";
+echo "<p>F                                              G<p>";
+echo "<p>What is the reason, when you really could have any man you want<p>";
+echo "<p>I don't see, what I have to offer.<p>";
+echo "<p>  F                                           G<p>";
+echo "<p>I shouldn't be in season, guess you could see I had potential.<p>";
+echo "<p>Fmaj7<p>";
+echo "<p>Do you know your my miracle?<p>";
+echo "<br>"; 
+echo "<p>[Chorus]<p>";
+echo "<p>             C                     Am<p>";
+echo "<p>I’m like a statue, stuck staring right at you<p>";
+echo "<p>                    F<p>";
+echo "<p>Got me frozen in my tracks.<p>";
+echo "<p>So amazed how you take me back<p>";
+echo "<p>G<p>";
+echo "<p>Each and everytime our love collapsed<p>";
+echo "<p>C                     Am<p>";
+echo "<p>Statue, stuck staring right at you<p>";
+echo "<p>            F<p>";
+echo "<p>So when I’m lost for words<p>";
+echo "<p>                       G<p>";
+echo "<p>Everytime I disappoint you<p>";
+echo "<p>Its just cause I can’t believe<p>";
+echo "<p>            C        Am<p>";
+echo "<p>That you're so beautiful. (Stuck like a statue)<p>";
+echo "<p>      F               G<p>";
+echo "<p>Don't wanna lose you, no. (Stuck like a statue)<p>";
+echo "<p>           C        Am<p>";
+echo "<p>And you're so beautiful. (Stuck like a statue)<p>";
+echo "<p>      F               G<p>";
+echo "<br>";
+echo "<p>Gsus G<p>";
+echo "<br>"; 
+echo "<p>[Bridge]<p>";
+echo "<p>Dm                         C/E<p>";
+echo "<p>    Every single day of my life I thank my lucky stars<p>";
+echo "<p>F                                 G<p>";
+echo "<p>    God really had to spend extra time when he sculptured your heart.<p>";
+echo "<p>Dm                         C/E                   Am   G<p>";
+echo "<p>    Cause there’s no explanation, cant solve the e---quation<p>";
+echo "<p>                     C/E    F<p>";
+echo "<p> Its like you love me more than I love myself.<p>";
+echo "<br>"; 
+echo "<p> [Chorus]<p>";
+echo "<p>              C                     Am<p>";
+echo "<p> I’m like a statue, stuck staring right at you<p>";
+echo "<p>                     F<p>";
+echo "<p> Got me frozen in my tracks.<p>";
+echo "<p> So amazed how you take me back<p>";
+echo "<p> G<p>";
+echo "<p> Each and everytime our love collapsed<p>";
+echo "<p> C                     Am<p>";
+echo "<p> Statue, stuck staring right at you<p>";
+echo "<p>             F<p>";
+echo "<p> So when I’m lost for words<p>";
+echo "<p>                        G<p>";
+echo "<p> Everytime I disappoint you<p>";
+echo "<p> Its just cause I can’t believe<p>";
+echo "<p>             C        Am<p>";
+echo "<p> That you're so beautiful. (You are the reason,)<p>";
+echo "<p> Stuck like a statue. (The reason for living,)<p>";
+echo "<p>       F               G<p>";
+echo "<p> Don't wanna lose you, no. (The reason for breathing)<p>";
+echo "<p> Stuck like a statue. (You're so beautiful)<p>";
+echo "<p>            C					Am<p>";
+echo "<p> And you're so beautiful. (And I want you to feel it)<p>";
+echo "<p> Stuck like a statue. (Cause so bad Im needing)<p>";
+echo "<p> F                     G<p>";
+echo "<p> Don't wanna lose you, no. (You're the reason for breathing)<p>";
+echo "<p> Stuck like a statue. (You're so beautiful)<p>";
+echo "<br>"; 
+echo "<p> N.C.<p>";
+echo "<p> Yeah, yeah, yeah, yeah<p>";
+echo "<br>"; 
+echo "<p> [Outro]<p>";
+echo "<p>        C<p>";
+echo "<p> When a day is said and done<p>";
+echo "<p>                      G<p>";
+echo "<p> In the middle of the night and your fast asleep, my love.<p>";
+echo "<p>           F<p>";
+echo "<p> telling myself Im the luckiest man alive<p>";
+
+?>
+<a href="index.php">Go Back</a>
+    </body>
+</html>
