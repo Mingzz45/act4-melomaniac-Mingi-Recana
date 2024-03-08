c3661f25aa32344fe6425cbdae8a0432026237f3
MingiStephannie <stepmingi@gmail.com>
2024-03-09 02:50:24 +0800

    Added index.php favorite songs webpage

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE <html>
+<html>
+</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Songs</title>
+    <style>
+        body {
+            background-image: url(123.jpg);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', sans-serif;
+            background-color: #ffffff;
+            background-size: cover;
+            font-size: 45;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        h1 {
+            color: #ffffff;
+            text-align: center;
+            -webkit-text-stroke:2px #000 ;
+         
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+            text-align: center;
+        }
+
+        li {
+            margin: 5px;
+        }
+
+        a {
+            text-decoration: none;
+            color:#ffffff;
+            font-weight: bold;
+            font-size: 45;
+          -webkit-text-stroke:2px #000;
+
+           
+        }
+        a:hover {
+            color: #ff33ff;
+        }
+        
+        </style>
+</head>
+<body>
+<img src="loh.png" alt="loh.png" class="logo">
+<h1> My Favorite Songs</h1>
+<li><a href="song1.php">Statue</a></11>
+<li><a href="song2.php">Take All The Love</a></li>
+<li><a href="song3.php">Talking To The Moon</a></li>
+<li><a href="song4.php">Araw-Araw</a></li>
+<li><a href="song5.php">Paninindigan Kita</a></li>
+
+</ul>
+</body>
+</html>
