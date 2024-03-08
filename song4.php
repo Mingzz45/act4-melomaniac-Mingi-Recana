a1823e7a6f22e452417aba3387a49c2941723d0d
MingiStephannie <stepmingi@gmail.com>
2024-03-09 02:54:07 +0800

    song4 with lyrics

---
+++ song4.php
@@ -0,0 +1,156 @@
+<!DOCTYPE <html>
+<html>
+</head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>song4</title>
+    <style>
+        body {
+            background-image: url(bg2.jpg);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', sans-serif;
+            background-color: #f2f2f2;
+            color: #333;
+            line-height: 1.15;
+            background-size: cover;
+            background-attachment: fixed;
+            font-size: 45;
+
+            
+        }
+        .content {
+            text-align: center;
+        }
+
+        h1 {
+            color: #ffffff;
+            -webkit-text-stroke:2px #000 ;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+            text-align: center;
+        }
+
+        li {
+            margin: 5px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #f3f;
+            font-weight: bold;
+            font-size: 30;
+        }
+
+        a:hover {
+            color: #ffffff;
+        }
+
+        p {
+            color: #ffffff;
+            margin-top: 1;
+            margin-bottom: 1;
+        }
+    </style>
+</head>
+<body>
+    <h1>Araw-Araw</h1>
+    <?php
+echo "<p>STRUMMING<p>";
+echo "<p>There is no strumming pattern for this song yet. Create and get +5 IQ<p>";
+echo "<p>N.C.<p>";
+echo "<p>Umaga na sa ating duyan<p>";
+echo "<p>N.C.<p>";
+echo "<p>'Wag nang mawawala<p>";
+echo "<p>N.C.<p>";
+echo "<p>Umaga na sa ating duyan<p>";
+echo "<p>N.C.<p>";
+echo "<p>Magmamahal, oh mahiwaga<p>";
+echo "<br>";  
+echo "<p>[Intro]<p>";
+echo "<p>B     F# E B<p>";
+echo "<br>";  
+echo "<p>[Verse 1]<p>";
+echo "<p>B             F#      E           B<p>";
+echo "<p>Matang magkakilala sa unang pagtagpo<p>";
+echo "<p>B           F#       E          B<p>";
+echo "<p>Paano dahan-dahang sinuyo ang puso?<p>";
+echo "<p>      F#                E<p>";
+echo "<p>Kay tagal ko nang nag-iisa,<p>";
+echo "<p>                  B<p>";
+echo "<p>Andyan ka lang pala<p>";
+echo "<br>";  
+echo "<p>[Chorus 1]<p>";
+echo "<p>      F#           E<p>";
+echo "<p>Mahiwaga, pipiliin ka<p>";
+echo "<p>         B<p>";
+echo "<p>Sa araw-araw<p>";
+echo "<p>      F#           E<p>";
+echo "<p>Mahiwaga ang nadarama<p>";
+echo "<p>           B<p>";
+echo "<p>Sayo'y malinaw<p>";
+echo "<br>";  
+echo "<p>[Verse 2]<p>";
+echo "<p>              F#<p>";
+echo "<p>Higit pa sa ligayang<p>";
+echo "<p>  E           B<p>";
+echo "<p>Hatid sa damdamin<p>";
+echo "<p>           F#<p>";
+echo "<p>Lahat naunawaan<p>";
+echo "<p>   E          B<p>";
+echo "<p>Sa lalim ng tingin<p>";
+echo "<br>";  
+echo "<p>[Chorus 2]<p>";
+echo "<p>      F#           E<p>";
+echo "<p>Mahiwaga, pipiliin ka<p>";
+echo "<p>         B<p>";
+echo "<p>Sa araw-araw<p>";
+echo "<p>      F#           E<p>";
+echo "<p>Mahiwaga ang nadarama<p>";
+echo "<p>           B<p>";
+echo "<p>Sayo'y malinaw<p>";
+echo "<br>";  
+echo "<p>[Bridge]<p>";
+echo "<p>A               E<p>";
+echo "<p>  Sa minsang pagbali ng hangin<p>";
+echo "<p>A          E<p>";
+echo "<p>  Hinila patungo sa akin<p>";
+echo "<p>A          E               A      E<p>";
+echo "<p>  Tanging ika'y iibiging wagas at buo<p>";
+echo "<p>  E       F#         B          F#<p>";
+echo "<p>Payapa sa yakap ng iyong hiwaga<p>";
+echo "<p>  E       F#         B<p>";
+echo "<p>Payapa sa yakap ng iyong<p>";
+echo "<br>";  
+echo "<p>[Instrumental]<p>";
+echo "<p>F# E B<p>";
+echo "<br>";  
+echo "<p>[Chorus 3]<p>";
+echo "<p>      F#           E<p>";
+echo "<p>Mahiwaga, pipiliin ka<p>";
+echo "<p>         B<p>";
+echo "<p>Sa araw-araw<p>";
+echo "<p>      F#           E<p>";
+echo "<p>Mahiwaga ang nadarama<p>";
+echo "<p>           B<p>";
+echo "<p>Sayo'y malinaw<p>";
+echo "<p>      F#                E<p>";
+echo "<p>Mahiwaga, 'wag nang mawala<p>";
+echo "<p>         B<p>";
+echo "<p>Sa araw-araw<p>";
+echo "<p>      F#           E<p>";
+echo "<p>Mahiwaga, pipiliin ka<p>";
+echo "<p>      B<p>";
+echo "<p>Araw-araw<p>";
+
+?>
+<a href="index.php">Go Back</a>
+    </body>
+</html>
